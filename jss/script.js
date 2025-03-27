8a2be120a9769921ba7c3ed8031c79aefb57a4e4
William05112 <liamscervino@gmail.com>
2025-03-27 02:07:43 +0100

    feat: esercizio da rivedere

---
+++ jss/script.js
@@ -0,0 +1,71 @@
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma (provate a implementare questa funzione con il ciclo for)
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 6.
+Generiamo un numero random (sempre da 1 a 6) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+Consigli del giorno
+1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+Domande da  farsi quando si crea una funzione:
+1. Come dovrebbe chiamarsi?
+2. Ho bisogno di parametri?
+3. Devo restituire un valore?
+4. Se sì, di che tipo?/*/
+
+// secondo metodo
+
+
+const parola = `ciao`;
+const parolaDivisa = parola.split("")
+console.log(parolaDivisa);
+
+let parolaInversa = "";
+for (let i = parolaDivisa.length - 1; i >= 0; i--) {
+    let una = parolaDivisa[i];
+    //console.log(una);
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//primo metodo
+// const parolaUtente = prompt("Inserisci una parola");
+// const parolaInversa = parolaUtente.split("").reverse().join("");
+// console.log(parolaUtente, parolaInversa);
+
+// if (parolaUtente === parolaInversa) {
+//     console.log("La parola è palindroma");
+// }else {
+//     console.log(`La parola ${parolaUtente} non è palindroma`);
+// }
+
