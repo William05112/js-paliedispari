7b8b8d9bcd9f40cdf40c99b065a12df86fc923e9
William05112 <liamscervino@gmail.com>
2025-03-29 10:28:23 +0100

    feat : palindroma completata

--- jss/script.js
+++ jss/script.js
@@ -1,61 +1,40 @@
-/*Palidroma
-Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma (provate a implementare questa funzione con il ciclo for)
-Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 6.
-Generiamo un numero random (sempre da 1 a 6) per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto.
-Consigli del giorno
-1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
-Domande da  farsi quando si crea una funzione:
-1. Come dovrebbe chiamarsi?
-2. Ho bisogno di parametri?
-3. Devo restituire un valore?
-4. Se sì, di che tipo?/*/
+// //Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma (provate a implementare questa funzione con il ciclo for)
 
 // secondo metodo
 
 
-const parola = `ciao`;
+const parola = prompt("Inserisci una parola");
+
 const parolaDivisa = parola.split("")
 console.log(parolaDivisa);
 
-let parolaInversa = "";
+// let parolaInversa = "";
+// for (let i = parola.length - 1; i >= 0; i--) {
+//     let una = parola[i];
+//     parolaInversa += una;
+//     //console.log(parolaInversa);
+// }
+// console.log(parolaInversa);
+
+
+
+
+let parolaInversa = [];
 for (let i = parolaDivisa.length - 1; i >= 0; i--) {
     let una = parolaDivisa[i];
-    //console.log(una);
-    
-    
+    parolaInversa.push(una);
 }
+console.log(parolaInversa);
+let parolaInversa1 = parolaInversa.join("");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (parola === parolaInversa1) {
+    console.log("La parola è palindroma");
+} else {
+    console.log(`La parola ${parola} non è palindroma`);
+}
 
 
 //primo metodo
@@ -68,4 +47,44 @@ for (let i = parolaDivisa.length - 1; i >= 0; i--) {
 // }else {
 //     console.log(`La parola ${parolaUtente} non è palindroma`);
 // }
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 6.
+// Generiamo un numero random (sempre da 1 a 6) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+// Domande da  farsi quando si crea una funzione:
+// 1. Come dovrebbe chiamarsi?
+// 2. Ho bisogno di parametri?
+// 3. Devo restituire un valore?
+// 4. Se sì, di che tipo?/*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
